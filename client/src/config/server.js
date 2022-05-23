f9a09be301cf338ae9b2ed6c0929b82d0b07725c
arifrahman9 <arifrhmn910@gmail.com>
2022-05-23 13:56:18 +0700

    changes url endpoint(#4)

--- client/src/config/server.js
+++ client/src/config/server.js
@@ -1,3 +1,2 @@
 // export const server = 'https://newsphone-blog-9fmtlwxay-arifrahman9.vercel.app/'
-// export const server = 'http://localhost:3000/'
-export const server = process.env.REACT_APP_BASE_URL
+export const server = 'http://localhost:3000/'
