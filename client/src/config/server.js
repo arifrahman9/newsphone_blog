aa329ee59a86cd82eaac532b633a51d402645086
arifrahman9 <arifrhmn910@gmail.com>
2022-05-23 13:36:23 +0700

    changes url endpoint(#2)

--- client/src/config/server.js
+++ client/src/config/server.js
@@ -1,2 +1,2 @@
-export const server = 'https://newsphone-blog.vercel.app/'
+export const server = 'https://newsphone-blog-9fmtlwxay-arifrahman9.vercel.app/'
 // export const server = 'http://localhost:3000/'
