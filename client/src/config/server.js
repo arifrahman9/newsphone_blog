621cf7f781238bd0c59c405aeb26f9b2465a4049
arifrahman9 <arifrhmn910@gmail.com>
2022-06-11 08:11:10 +0700

    change endpoint and setup deployment

--- client/src/config/server.js
+++ client/src/config/server.js
@@ -1,4 +1,3 @@
-// export const baseDomain = `${window.location.protocol}//${window.location.host}`
-// export const server = `${baseDomain}/`
-export const server = process.env.REACT_APP_BASE_URL
+// export const server = process.env.REACT_APP_BASE_URL
+export const server = 'https://newsphoneblog.herokuapp.com/'
 // export const server = "http://localhost:3000/"
